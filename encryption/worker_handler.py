16ff242392ba793388f51750fc3a5cbed2dfd507
Martin <martinpop06@gmail.com>
2025-12-11 17:02:51 +0100

    made test_clean_up_zombies_on_failure pass 👍

--- encryption/worker_handler.py
+++ encryption/worker_handler.py
@@ -1,14 +1,27 @@
 from multiprocessing import Process
 from .workers import encryption_worker
 
+import logging
+logger = logging.getLogger(__name__)
+
 def start_workers(worker_count, config):
-
+    """
+    Starts encryption workers (processes)
+    :param worker_count: how many workers
+    :param config: worker configuration
+    :return: list of workers
+    """
     workers = []
-    for _ in range(worker_count):
-        process = Process(target=encryption_worker, args=(config,))
-        workers.append(process)
+    try:
+        for _ in range(worker_count):
+            process = Process(target=encryption_worker, args=(config,))
+            workers.append(process)
 
-        process.start()
+            process.start()
+
+    except Exception as e:
+        logger.error(f'Failed to start encryption workers {e}', exc_info=True)
+        for process in workers:
+            process.terminate()
 
     return workers
-
